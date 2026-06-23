a5053fa81d012ee0da64769fec58f2cdb820d71e
attaryu <mattarannaufal@gmail.com>
2026-06-23 12:21:30 +0700

    fix: improve environment variable loading and error handling in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,30 @@
 using Microsoft.EntityFrameworkCore;
-using TodoList.Api.src.Models;
-using DotNetEnv;
 using TodoList.Api.src.Data;
-using Microsoft.Extensions.Options;
+using DotNetEnv;
 
-Env.Load();
+string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+    ?? Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")
+    ?? "Development";
+
+string envFile = $".env.{environment}";
+
+if (!File.Exists(envFile) && environment != "Production")
+{
+    throw new InvalidOperationException("Failed to find .env.* file. Ensure it's present in the application root!");
+}
+else if (environment != "Production")
+{
+    Env.Load(envFile);
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Configuration.AddEnvironmentVariables();
 
-var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")
-    ?? builder.Configuration.GetConnectionString("DefaultConnection");
-
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options
         .UseNpgsql(connectionString)
