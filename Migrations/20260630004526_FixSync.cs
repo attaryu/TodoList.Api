a26382a14b82407ad63a59ef106c712e3fc99428
attaryu <mattarannaufal@gmail.com>
2026-06-30 07:55:27 +0700

    fix(todo): aligning the database migration schema with the model

---
+++ Migrations/20260630004526_FixSync.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TodoList.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSync : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // sync the database schema
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // sync the database schema
+        }
+    }
+}
