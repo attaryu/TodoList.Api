d8b89115f89217de8d28d582ca32dd94cdf59482
attaryu <mattarannaufal@gmail.com>
2026-07-03 07:04:29 +0700

    feat(email-verification): update email verification link and expiration time in SendEmailVerificationUseCase

--- src/TodoList.Api/Features/Auth/Core/UseCases/SendEmailVerificationUseCase.cs
+++ src/TodoList.Api/Features/Auth/Core/UseCases/SendEmailVerificationUseCase.cs
@@ -11,7 +11,8 @@ public class SendEmailVerificationUseCase(
     IUserRepository userRepository,
     IEmailVerificationRepository emailVerificationRepository,
     IUnitOfWork unitOfWork,
-    ISendEndpointProvider sendEndpointProvider
+    ISendEndpointProvider sendEndpointProvider,
+    IConfiguration configuration
 )
 {
     private readonly IUserRepository _userRepository = userRepository;
@@ -19,6 +20,7 @@ public class SendEmailVerificationUseCase(
         emailVerificationRepository;
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
     private readonly ISendEndpointProvider _sendEndpointProvider = sendEndpointProvider;
+    private readonly IConfiguration _configuration = configuration;
 
     public async Task ExecuteAsync(int userId)
     {
@@ -45,19 +47,25 @@ public class SendEmailVerificationUseCase(
         {
             UserId = userId,
             Token = token,
-            ExpiresAt = DateTime.UtcNow.AddHours(24),
+            ExpiresAt = DateTime.UtcNow.AddMinutes(10),
             CreatedAt = DateTime.UtcNow,
         };
 
         await _emailVerificationRepository.AddAsync(verification);
         await _unitOfWork.SaveChangesAsync();
 
+        var domain = _configuration["DOMAIN"];
+
         // Construct HTML email body
         var emailBody = $"""
             <h1>Verify Your Email</h1>
             <p>Please use the following token to verify your email address:</p>
-            <p><strong>{token}</strong></p>
-            <p>This token will expire at {verification.ExpiresAt:yyyy-MM-dd HH:mm:ss} UTC.</p>
+            <button style="padding: 10px 20px; font-size: 16px; background-color: black; color: white; border: none; border-radius: 5px; cursor: pointer;">
+                <a href="{domain}/verify-email?token={verification.Token}" style="color: white; text-decoration: none;" target="_blank">
+                    Verify Email
+                </a>
+            </button>
+            <p>This link will expire in 10 minutes from {DateTime.UtcNow:dd MMM yyyy HH:mm:ss} UTC.</p>
             """;
 
         // Send message to mapped endpoint
