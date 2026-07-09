300b57b779ee684769ffb61a92c6d70a2db17b46
attaryu <mattarannaufal@gmail.com>
2026-07-09 14:02:01 +0700

    feat(soft-delete): implement soft delete functionality in AppDbContext

--- src/TodoList.Api/Infrastructure/DataContext/AppDbContext.cs
+++ src/TodoList.Api/Infrastructure/DataContext/AppDbContext.cs
@@ -1,6 +1,8 @@
+using System.Linq.Expressions;
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using TodoList.Api.Domain.Entities;
+using TodoList.Api.Domain.Entities.Base;
 
 namespace TodoList.Api.Infrastructure.DataContext;
 
@@ -30,5 +32,49 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
             .WithMany()
             .HasForeignKey(e => e.UserId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            if (typeof(ISoftDelete).IsAssignableFrom(entityType.ClrType))
+            {
+                modelBuilder
+                    .Entity(entityType.ClrType)
+                    .HasQueryFilter(ConvertFilterExpression(entityType.ClrType));
+            }
+        }
+    }
+
+    public override int SaveChanges()
+    {
+        ApplySoftDelete();
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        ApplySoftDelete();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    private void ApplySoftDelete()
+    {
+        foreach (var entry in ChangeTracker.Entries<ISoftDelete>())
+        {
+            if (entry.State == EntityState.Deleted)
+            {
+                entry.State = EntityState.Modified;
+                entry.Entity.IsActive = false;
+                entry.Entity.DeletedDate = DateTimeOffset.UtcNow;
+            }
+        }
+    }
+
+    private static LambdaExpression ConvertFilterExpression(Type type)
+    {
+        var parameter = Expression.Parameter(type, "e");
+        var property = Expression.Property(parameter, nameof(ISoftDelete.IsActive));
+        var body = Expression.Equal(property, Expression.Constant(true));
+
+        return Expression.Lambda(body, parameter);
     }
 }
